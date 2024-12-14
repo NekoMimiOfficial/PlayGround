aa266fdac806216eed7e4cb8a3c11ee23a6c004a
NekoMimiOfficial <nekomimi@tilde.team>
2024-12-14 19:59:21 +0300

    basic sdl2 window example

---
+++ c++/sdl2/CMakeFiles/packages.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/packages.dir/main.cpp.o"
+  "CMakeFiles/packages.dir/main.cpp.o.d"
+  "bin/Linux64/packages"
+  "bin/Linux64/packages.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/packages.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
