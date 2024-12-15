6e9a5854f8b8f0d04d5d46ac297d1cbc8f5b45af
NekoMimiOfficial <nekomimi@tilde.team>
2024-12-15 17:39:13 +0300

    simple threading example

---
+++ c++/threads/CMakeFiles/threading.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/threading.dir/main.cpp.o"
+  "CMakeFiles/threading.dir/main.cpp.o.d"
+  "bin/Linux64/threading"
+  "bin/Linux64/threading.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/threading.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
