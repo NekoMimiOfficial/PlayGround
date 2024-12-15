bbc34ac64e947270d05a2e185e0f7ffaefa2d47c
NekoMimiOfficial <nekomimi@tilde.team>
2024-12-15 17:58:10 +0300

    added a rounded cornered box renderer

--- c++/sdl2/main.cpp
+++ c++/sdl2/main.cpp
@@ -11,6 +11,79 @@
 // You shouldn't really use this statement, but it's fine for small programs
 using namespace std;
 
+
+// draws a rounded box with…
+// corner radius of ‘r’
+// width of ‘w’
+// and height of ‘h’
+// draws the box right and down of…
+// x-offset xo
+// y-offset yo
+
+// returns 0 if 2*r is bigger than w or h
+// and draws nothing
+// returns 1 on success
+
+int fill_rounded_box_b( SDL_Surface* dst, int xo, int yo, int w, int h, int r, Uint32 color )
+{
+  int yd = dst->pitch / dst->format->BytesPerPixel;
+  Uint32* pixels = NULL;
+
+  int x,y,i,j;
+  int rpsqrt2 = (int) (r / sqrt( 2 ) );
+  double r2 = r*r;
+
+  w /= 2;
+  h /= 2;
+
+  xo += w;
+  yo += h;
+
+  w -= r;
+  h -= r;
+
+  if( w < 0 || h < 0 ){
+  return 0;}
+
+  SDL_LockSurface( dst );
+  pixels = (Uint32*)( dst->pixels );
+
+  int sy = (yo-h)*yd;
+  int ey = (yo+h)*yd;
+  int sx = (xo-w);
+  int ex = (xo+w);
+  for( i = sy; i<=ey; i+=yd ){
+  for( j = sx-r; j<=ex+r; j++ ){
+  pixels[i+j] = color;}}
+
+  int d = -r;
+  int x2m1 = -1;
+  y = r;
+  for( x=0; x <= rpsqrt2; x++ ) {
+  x2m1 += 2;
+  d+= x2m1;
+  if( d >= 0 ) {
+  y = y - 1;
+  d -= (y*2);
+  }
+
+  for( i=sx-x; i<=ex+x; i++ )
+    pixels[sy-y*yd + i] = color;
+
+  for( i=sx-y; i<=ex+y; i++ )
+    pixels[sy-x*yd + i] = color;
+
+  for( i=sx-y; i<=ex+y; i++ )
+    pixels[ey+x*yd + i] = color;
+
+  for( i=sx-x; i<=ex+x; i++ )
+    pixels[ey+y*yd + i] = color;
+  }
+  SDL_UnlockSurface( dst );
+  return 1;
+}
+
+
 void quit_SDL(SDL_Window* win)
 {
   // Destroy the window. This will also destroy the surface
@@ -61,9 +134,8 @@ int main(int argc, char** args) {
   SDL_Rect header;
   header.x = 0; header.y = 0; header.w = 720; header.h = 60;
   SDL_FillRect(winSurface, &header, SDL_MapRGB(winSurface->format, 255, 200, 200));
-
-	// Update the window display
-	SDL_UpdateWindowSurface( window );
+  fill_rounded_box_b(winSurface, 10, 70, 160, 320, 12, SDL_MapRGB(winSurface->format, 255, 100, 100));
+  fill_rounded_box_b(winSurface, 180, 70, 530, 320, 12, SDL_MapRGB(winSurface->format, 255, 100, 100));
 
 	// Wait
 	/*system("sleep 4");*/
@@ -75,6 +147,10 @@ int main(int argc, char** args) {
   //program loop
   while (quit_ident)
   {
+
+    // Update the window display
+    SDL_UpdateWindowSurface( window );
+
     SDL_PollEvent(&test_event);
     switch (test_event.type)
     {
